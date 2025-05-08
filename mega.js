cf3e3c1e8f24901f2ce0858291a0c0b516333ae2
McrayNick <nickhunter509@gmail.com>
2025-05-08 22:31:07 +0300

    Update mega.js

--- mega.js
+++ mega.js
@@ -2,8 +2,8 @@ import * as mega from 'megajs';
 
 // Mega authentication credentials
 const auth = {
-    email: 'lahepan437@owlny.com', // Replace with your Mega email
-    password: 'lahepan437@', // Replace with your Mega password
+    email: 'nickhunter383@gmail.com, // Replace with your Mega email
+    password: 'nick12@#nick12£', // Replace with your Mega password
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 };
 
