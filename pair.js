7267ecc0c5a187daa337c9b5ea14bead904729ea
McrayNick <nickhunter509@gmail.com>
2025-05-20 12:40:05 +0300

    Update pair.js

--- pair.js
+++ pair.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import fs from 'fs';
 import pino from 'pino';
-import { makeWASocket, useMultiFileAuthState, delay, makeCacheableSignalKeyStore, Browsers, jidNormalizedUser } from 'baileys-elite';
+import { makeWASocket, useMultiFileAuthState, delay, makeCacheableSignalKeyStore, Browsers, jidNormalizedUser } from '@whiskeysockets/baileys';
 import { upload } from './mega.js';
 
 const router = express.Router();
